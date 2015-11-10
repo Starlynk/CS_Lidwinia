4d2f442379be148442867a9e3c3e54812840170f
Starlynk <martijnkleuskens@gmail.com>
2015-11-10 20:51:58 +0100

    Update CS_Lidwinia_beta.user.js

--- CS_Lidwinia_beta.user.js
+++ CS_Lidwinia_beta.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         CS_Lidwinia_beta
-// @version      0.2
+// @version      0.21
 // @author       M. Kleuskens
 // @include      *cyclingsimulator.com*
 // @grant        none
@@ -144,7 +144,7 @@ if(window.location.search.indexOf("Break") > -1)
             if ($(rlist_riders[l]).attr("onClick").indexOf(riderID) >-1)
                 {
                     var DP = $(rlist_skills[l*12+9]).text();
-                    var rb_rise = Math.floor(Math.min(((rb_doc_impact*120*(100-DP))/240),25));
+                    var rb_rise = Math.min(Math.floor(Math.min(((rb_doc_impact*120*(100-DP))/240),25)),99-DP);
                     var rb_hours = Math.ceil((rb_rise*240)/rb_doc_impact/(100-DP));
                     var rb_days = Math.floor(rb_hours/24);
                     rb_hours = rb_hours-(rb_days*24);
